17b43027c9f51c3ee5e83fefbcc645af0bc711ad
zakaahmed1 <zakaahmed1@hotmail.com>
2025-08-09 17:05:09 +0100

    Updated values to most recent statistics

--- public/data/game_data.ts
+++ public/data/game_data.ts
@@ -7,14 +7,14 @@ export type Profile = {
 };
 
 export const gameData: Profile[] = [
-  { name: "Cristiano Ronaldo", description: "footballer", country: "Portugal", follower_count: 605, image: "/images/cristiano.jpg" },
-  { name: "Lionel Messi", description: "footballer", country: "Argentina", follower_count: 504, image: "/images/messi.webp"   },
-  { name: "Selena Gomez", description: "musician and actress", country: "USA", follower_count: 429, image: "/images/selenagomez.webp" },
-  { name: "Kylie Jenner", description: "media personality", country: "USA", follower_count: 400, image: "/images/kyliejenner.webp"   },
-  { name: "Dwayne Johnson", description: "actor and wrestler", country: "USA", follower_count: 392, image: "/images/dwaynejohnson.avif" },
-  { name: "Ariana Grande", description: "singer and actress", country: "USA", follower_count: 380, image: "/images/arianagrande.webp"   },
-  { name: "Kim Kardashian", description: "media personality", country: "USA", follower_count: 365, image: "/images/kimkardashian.avif"   },
-  { name: "Taylor Swift", description: "singer-songwriter", country: "USA", follower_count: 322, image: "/images/taylorswift.avif"   },
-  { name: "Virat Kohli", description: "cricketer", country: "India", follower_count: 278, image: "/images/virat.webp" },
-  { name: "Neymar Jr", description: "footballer", country: "Brazil", follower_count: 220, image: "/images/neymar.jpg" },
+  { name: "Cristiano Ronaldo", description: "Footballer", country: "Portugal", follower_count: 662, image: "/images/cristiano.jpg" },
+  { name: "Lionel Messi", description: "Footballer", country: "Argentina", follower_count: 507, image: "/images/messi.webp"   },
+  { name: "Selena Gomez", description: "Musician and Actress", country: "USA", follower_count: 418, image: "/images/selenagomez.webp" },
+  { name: "Kylie Jenner", description: "Media Personality", country: "USA", follower_count: 393, image: "/images/kyliejenner.webp"   },
+  { name: "Dwayne Johnson", description: "Actor and Wrestler", country: "USA", follower_count: 392, image: "/images/dwaynejohnson.avif" },
+  { name: "Ariana Grande", description: "Singer and Actress", country: "USA", follower_count: 375, image: "/images/arianagrande.webp"   },
+  { name: "Kim Kardashian", description: "Media Personality", country: "USA", follower_count: 356, image: "/images/kimkardashian.avif"   },
+  { name: "Taylor Swift", description: "Singer-songwriter", country: "USA", follower_count: 281, image: "/images/taylorswift.avif"   },
+  { name: "Virat Kohli", description: "Cricketer", country: "India", follower_count: 274, image: "/images/virat.webp" },
+  { name: "Neymar Jr", description: "Footballer", country: "Brazil", follower_count: 231, image: "/images/neymar.jpg" },
 ];
