8d334d821e0f6c8a6be278cc5fa2a4d8b365c741
zakaahmed1 <zakaahmed1@hotmail.com>
2025-08-10 14:48:32 +0100

    Removed white border by creating a globals.css file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 // pages/_app.tsx
+import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
-import '../styles/Home.module.css';
 
-function MyApp({ Component, pageProps }) {
+// Only global CSS belongs here
+import '../styles/globals.css';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
@@ -10,5 +13,3 @@ function MyApp({ Component, pageProps }) {
     </>
   );
 }
-
-export default MyApp;
